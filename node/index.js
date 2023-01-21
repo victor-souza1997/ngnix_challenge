33d5af5af39bd8ae320d6c005e0ffe265d93ac87
João <victor-souza1997@hotmail.com>
2023-01-21 18:25:41 -0400

    select working

--- node/index.js
+++ node/index.js
@@ -16,7 +16,23 @@ const sql = `INSERT INTO people(name) values('Victor')`
 connection.query(sql)
 connection.end()
 
+
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')})
+    const mysql = require('mysql2')
+    const connection = mysql.createConnection(config)
+    var result;
+    var result = await connection.query('SELECT * FROM people', (err, rows, fields) => {
+        if (err) throw err
+        result = rows;
+        console.log('The solution is: ', result[0].name)
+      })
+    connection.end()
+    console.log(result)
+    
+    res.send('hello'
+    //res.send('<h1>Full Cycle</h1><h2>hello</h2>'
+    )
+ }
+    )
 
 app.listen(port, ()=>{console.log('Rodando na porta '+port)})
